4c9b9f67978bf79f9ccbb6a81fa6d054d358f27b
Damian Senn <senn@puzzle.ch>
2016-10-21 17:44:56 +0200

    Add page classes to body

--- app/app.js
+++ app/app.js
@@ -13,4 +13,16 @@ const App = Ember.Application.extend({
 
 loadInitializers(App, config.modulePrefix)
 
+Ember.Route.reopen({
+  className() {
+    return `page-${Ember.String.dasherize(this.routeName.replace(/\./g, '-'))}`
+  },
+  activate() {
+    document.body.classList.add(this.className())
+  },
+  deactivate() {
+    document.body.classList.remove(this.className())
+  }
+})
+
 export default App
--- app/routes/intro.js
+++ app/routes/intro.js
@@ -5,14 +5,6 @@ const { Route, inject } = Ember
 export default Route.extend({
   intro: inject.service(),
 
-  activate() {
-    document.body.classList.add('page-intro')
-  },
-
-  deactivate() {
-    document.body.classList.remove('page-intro')
-  },
-
   actions: {
     play() {
       this.set('intro.isCompleted', true)
