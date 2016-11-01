3addc1762274b3a5e59efa068452975926936967
Damian Senn <senn@puzzle.ch>
2016-11-01 15:56:18 +0100

    Add autoprefixer

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,10 +1,18 @@
-/*jshint node:true*/
-/* global require, module */
-var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+/* eslint-env node */
+const EmberApp = require('ember-cli/lib/broccoli/ember-app')
 
 module.exports = function(defaults) {
-  var app = new EmberApp(defaults, {
-  });
+  let app = new EmberApp(defaults, {
+    autoprefixer: {
+      browsers: [
+        'Chrome >= 50',
+        'Safari >= 9',
+        'iOS >= 9',
+        'Firefox ESR',
+        'Last 3 Firefox versions'
+      ]
+    }
+  })
 
-  return app.toTree();
-};
+  return app.toTree()
+}
