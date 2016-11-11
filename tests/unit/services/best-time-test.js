bcb2ee668a4b639b4cf13f2dc2e3b821f702ce3d
Damian Senn <senn@puzzle.ch>
2016-11-11 15:56:51 +0100

    Best time service tests

--- tests/unit/services/best-time-test.js
+++ tests/unit/services/best-time-test.js
@@ -1,12 +1,72 @@
-import { moduleFor, test } from 'ember-qunit';
+import { moduleFor, test } from 'ember-qunit'
 
 moduleFor('service:best-time', 'Unit | Service | best time', {
-  // Specify the other units that are required for this test.
-  // needs: ['service:foo']
-});
+})
 
-// Replace this with your real tests.
-test('it exists', function(assert) {
-  let service = this.subject();
-  assert.ok(service);
-});
+test('it holds the best time in localstorage', function(assert) {
+  localStorage.clear()
+  localStorage.setItem('puzzle-slide-best-time', 100)
+
+  let service = this.subject()
+
+  assert.equal(service.get('value'), 100)
+})
+
+test('it sets value into localstorage', function(assert) {
+  localStorage.clear()
+
+  let service = this.subject()
+
+  service.set('value', 100)
+
+  assert.equal(localStorage.getItem('puzzle-slide-best-time'), '100')
+})
+
+test('it sets a date when a time is set', function(assert) {
+  localStorage.clear()
+
+  let service = this.subject()
+
+  service.set('value', 100)
+
+  assert.ok(service.get('date') instanceof Date, 'value gets a timestamp')
+  assert.ok(localStorage.getItem('puzzle-slide-best-time-date'))
+})
+
+test('it stores the latest result', function(assert) {
+  localStorage.clear()
+  localStorage.setItem('puzzle-slide-latest-time', 100)
+
+  let service = this.subject()
+
+  assert.equal(service.get('latest'), 100)
+})
+
+test('it sets latest into localstorage', function(assert) {
+  localStorage.clear()
+
+  let service = this.subject()
+
+  service.set('latest', 100)
+
+  assert.equal(localStorage.getItem('puzzle-slide-latest-time'), '100')
+})
+
+test('it stores the best time id', function(assert) {
+  localStorage.clear()
+  localStorage.setItem('puzzle-slide-best-time-id', 100)
+
+  let service = this.subject()
+
+  assert.equal(service.get('id'), '100')
+})
+
+test('it sets best time id into localstorage', function(assert) {
+  localStorage.clear()
+
+  let service = this.subject()
+
+  service.set('id', 100)
+
+  assert.equal(localStorage.getItem('puzzle-slide-best-time-id'), '100')
+})
